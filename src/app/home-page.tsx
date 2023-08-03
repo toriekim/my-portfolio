b70b1387c2a64be8c6096df49266a828b4c56575
Torie <vkim20@gmail.com>
2023-08-03 15:15:42 -0400

    fix: no escaped chars

--- src/app/home-page.tsx
+++ src/app/home-page.tsx
@@ -11,11 +11,8 @@ const HomePage = () => {
         alt="landing profile picture of Torie sitting in dramatic bushes"
         className="object-center"
       />
-      <h1
-        className={`font-display absolute bottom-1/3 z-50 mx-5 text-center text-8xl
-        `}
-      >
-        Hi, I'm Torie
+      <h1 className="font-display absolute bottom-1/3 z-50 mx-5 text-center text-8xl">
+        Hi, I&apos;m Torie
       </h1>
     </div>
   );
