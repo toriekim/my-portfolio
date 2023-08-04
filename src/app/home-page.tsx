33545b01387bed65fc08f2b3c44a23cd5bf65d39
Torie <vkim20@gmail.com>
2023-08-04 11:41:28 -0400

    quick fix: landing header to white

--- src/app/home-page.tsx
+++ src/app/home-page.tsx
@@ -11,7 +11,7 @@ const HomePage = () => {
         alt="landing profile picture of Torie sitting in dramatic bushes"
         className="object-center"
       />
-      <h1 className="font-display absolute bottom-1/3 z-50 mx-5 text-center text-8xl">
+      <h1 className="font-display absolute bottom-1/3 z-50 mx-5 text-center text-8xl text-white">
         Hi, I&apos;m Torie
       </h1>
     </div>
