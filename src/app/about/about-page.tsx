34f317ce57db1264222c6c8ba6eacd0ecb6761fc
Torie <vkim20@gmail.com>
2023-11-09 16:05:23 -0500

    add: about pages

---
+++ src/app/about/about-page.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <div className="flex h-screen flex-col items-center gap-4">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import AboutPage from './about-page';
+
+const Page = () => {
+  return (
+    <main className="mx-14">
+      <AboutPage />
+    </main>
+  );
+};
+
+export default Page;
