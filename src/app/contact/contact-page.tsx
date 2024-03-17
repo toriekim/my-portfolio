a9304acfd584bf5aa1e060752f52468485fd2acc
Torie <vkim20@gmail.com>
2024-03-17 18:45:23 -0400

    add: Contact form & components

---
+++ src/app/contact/contact-page.tsx
@@ -0,0 +1,31 @@
+import ContactForm from '@/components/ContactForm';
+
+const ContactPage = () => {
+  return (
+    <div className="flex flex-col items-start justify-start gap-24 py-16 md:flex-row md:gap-48">
+      <div className="flex w-full grow flex-col items-start justify-start gap-[60px]">
+        {/* left-side text */}
+        <div className="flex flex-col items-start justify-start gap-10">
+          <h3 className="font-display text-[32px] font-bold leading-[42px] text-magentaPink">
+            Let’s work together!
+          </h3>
+          <p className="text-[17px] font-normal leading-[27px]">
+            This is a template Figma file, turned into code using Anima. Learn
+            more at AnimaApp.com This is a template Figma file, turned into code
+            using Anima. Learn more at AnimaApp.com
+          </p>
+        </div>
+        {/* <div className="relative h-9">
+            <div className="absolute left-[147px] top-0 inline-flex h-9 w-9 flex-col items-start justify-start" />
+            <div className="absolute left-[196px] top-0 inline-flex h-9 w-9 flex-col items-start justify-start" />
+            <div className="absolute left-[98px] top-0 inline-flex h-9 w-9 flex-col items-start justify-start" />
+            <div className="absolute left-[49px] top-0 inline-flex h-9 w-9 flex-col items-start justify-start" />
+            <div className="absolute left-0 top-0 inline-flex h-9 w-9 flex-col items-start justify-start" />
+          </div> */}
+      </div>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default ContactPage;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import ContactPage from './contact-page';
+
+const Page = () => {
+  return (
+    <main className="mx-14">
+      <ContactPage />
+    </main>
+  );
+};
+
+export default Page;
